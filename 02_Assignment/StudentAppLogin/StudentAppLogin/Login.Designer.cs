8e1d71032173d9deae7f14366c4777c8d38ecadf
RautMaya <suryawanshimaya28@gmail.com>
2024-02-14 19:49:35 +0530

    Practice Assignment

---
+++ 02_Assignment/StudentAppLogin/StudentAppLogin/Login.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace StudentAppLogin
+{
+    partial class frm_Login_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Login_Form = new System.Windows.Forms.Label();
+            this.lbl_Username = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.btn_Submit = new System.Windows.Forms.Button();
+            this.tb_Username = new System.Windows.Forms.TextBox();
+            this.tb_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Login_Form
+            // 
+            this.lbl_Login_Form.AutoSize = true;
+            this.lbl_Login_Form.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lbl_Login_Form.Font = new System.Drawing.Font("Georgia", 30.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Login_Form.ForeColor = System.Drawing.Color.Black;
+            this.lbl_Login_Form.Location = new System.Drawing.Point(279, 32);
+            this.lbl_Login_Form.Name = "lbl_Login_Form";
+            this.lbl_Login_Form.Size = new System.Drawing.Size(290, 59);
+            this.lbl_Login_Form.TabIndex = 0;
+            this.lbl_Login_Form.Text = "Login Form";
+            // 
+            // lbl_Username
+            // 
+            this.lbl_Username.AutoSize = true;
+            this.lbl_Username.Font = new System.Drawing.Font("Georgia", 20.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Username.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lbl_Username.Location = new System.Drawing.Point(81, 167);
+            this.lbl_Username.Name = "lbl_Username";
+            this.lbl_Username.Size = new System.Drawing.Size(177, 41);
+            this.lbl_Username.TabIndex = 1;
+            this.lbl_Username.Text = "Username";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Font = new System.Drawing.Font("Georgia", 20.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Password.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lbl_Password.Location = new System.Drawing.Point(89, 295);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(166, 41);
+            this.lbl_Password.TabIndex = 2;
+            this.lbl_Password.Text = "Password";
+            // 
+            // btn_Submit
+            // 
+            this.btn_Submit.BackColor = System.Drawing.Color.White;
+            this.btn_Submit.Font = new System.Drawing.Font("Georgia", 20.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Submit.ForeColor = System.Drawing.Color.Black;
+            this.btn_Submit.Location = new System.Drawing.Point(289, 424);
+            this.btn_Submit.Name = "btn_Submit";
+            this.btn_Submit.Size = new System.Drawing.Size(224, 54);
+            this.btn_Submit.TabIndex = 3;
+            this.btn_Submit.Text = "Submit";
+            this.btn_Submit.UseVisualStyleBackColor = false;
+            this.btn_Submit.Click += new System.EventHandler(this.btn_Submit_Click);
+            // 
+            // tb_Username
+            // 
+            this.tb_Username.BackColor = System.Drawing.Color.White;
+            this.tb_Username.Location = new System.Drawing.Point(514, 164);
+            this.tb_Username.MaxLength = 12;
+            this.tb_Username.Name = "tb_Username";
+            this.tb_Username.Size = new System.Drawing.Size(327, 46);
+            this.tb_Username.TabIndex = 1;
+            // 
+            // tb_Password
+            // 
+            this.tb_Password.BackColor = System.Drawing.Color.White;
+            this.tb_Password.Location = new System.Drawing.Point(514, 295);
+            this.tb_Password.MaxLength = 8;
+            this.tb_Password.Name = "tb_Password";
+            this.tb_Password.PasswordChar = '$';
+            this.tb_Password.Size = new System.Drawing.Size(327, 46);
+            this.tb_Password.TabIndex = 2;
+            // 
+            // frm_Login_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 39F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(916, 532);
+            this.Controls.Add(this.tb_Password);
+            this.Controls.Add(this.tb_Username);
+            this.Controls.Add(this.btn_Submit);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_Username);
+            this.Controls.Add(this.lbl_Login_Form);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7);
+            this.Name = "frm_Login_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Login_Form;
+        private System.Windows.Forms.Label lbl_Username;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Button btn_Submit;
+        private System.Windows.Forms.TextBox tb_Username;
+        private System.Windows.Forms.TextBox tb_Password;
+    }
+}
+
---
+++ 02_Assignment/StudentAppLogin/StudentAppLogin/Student Details.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentAppLogin
+{
+    public partial class frm_Student_Details : Form
+    {
+        public frm_Student_Details()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection DBCon = new SqlConnection(@"Data Source=DESKTOP-38U3S54;Initial Catalog=StudentAppLogin_DB;Integrated Security=True");
+        void S_Con_Open()
+        {
+            if (DBCon.State!=ConnectionState.Open)
+            {
+                DBCon.Open();
+
+            }
+        }
+        void S_Con_Close()
+        {
+            if (DBCon.State!=ConnectionState.Closed)
+            {
+                DBCon.Close();
+
+            }
+        }
+        private void btn_Student_List_Click(object sender, EventArgs e)
+        {
+            frm_Student_List Obj = new frm_Student_List();
+            Obj.Show();
+            this.Hide();
+           
+        }
+        void Clear_Controls()
+        {
+            
+            tb_Full_Name.Clear();
+            tb_Mobile_No.Clear();
+
+            dtp_Date_Of_Birth.ResetText();
+            cmb_Course.SelectedIndex = -1;
+        }
+
+
+        private void btn_Refresh_Click(object sender, EventArgs e)
+        {
+            Clear_Controls();
+        }
+
+        int AutoIncriment()
+        {
+            S_Con_Open();
+
+            SqlCommand Cmd = new SqlCommand();
+
+            Cmd.Connection = DBCon;
+            Cmd.CommandText = "Select Count(*) From Student_Details";
+
+            int RollNo = Convert.ToInt32(Cmd.ExecuteScalar());
+
+            Cmd.Dispose();
+
+            if (RollNo > 0)
+            {
+                Cmd.Connection = DBCon;
+                Cmd.CommandText = "Select MAX(Roll_No) From Student_Details";
+
+                RollNo = Convert.ToInt32(Cmd.ExecuteScalar()) + 1;
+
+            }
+            else
+            {
+                RollNo = 101;
+            }
+
+            S_Con_Close();
+            return RollNo;
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            S_Con_Open();
+
+            if (tb_Roll_No.Text!=""&&tb_Full_Name.Text!=""&&tb_Mobile_No.Text!=""&&cmb_Course.Text!="")
+            {
+                SqlCommand Cmd = new SqlCommand();
+
+                Cmd.Connection = DBCon;
+                Cmd.CommandText = "Insert Into Student_Details(Roll_No,Full_Name,Date_Of_Birth,Mobile_No,Course) Values(@RNo,@Nm,@Dob,@MNo,@Course)";
+
+                Cmd.Parameters.Add("RNo", SqlDbType.Int).Value = tb_Roll_No.Text;
+                Cmd.Parameters.Add("Nm", SqlDbType.VarChar).Value = tb_Full_Name.Text;
+                Cmd.Parameters.Add("Dob", SqlDbType.Date).Value = dtp_Date_Of_Birth.Text;
+                Cmd.Parameters.Add("MNo", SqlDbType.Decimal).Value = tb_Mobile_No.Text;
+                Cmd.Parameters.Add("Course", SqlDbType.NVarChar).Value = cmb_Course.Text;
+
+                Cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Student Information Saved Succesfully", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                Clear_Controls();
+
+            }
+            else
+            {
+                MessageBox.Show("1st Fill all Fields", "Incomplete Data ", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            S_Con_Close();
+
+        }
+        private void frm_Student_Details_Load(object sender, EventArgs e)
+        {
+            lbl_LoginUserName.Text = "Welcome " + Shared_Content.LoggedInUser;
+
+            tb_Roll_No.Text = Convert.ToString(AutoIncriment());
+        }
+
+
+
+        private void btn_Log_Out_Click(object sender, EventArgs e)
+        {
+            frm_Login_Form Obj = new frm_Login_Form();
+            Obj.Show();
+            this.Hide();
+        }
+
+        private void Only_Numeric(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsDigit(e.KeyChar)||(e.KeyChar==(char)Keys.Back)))
+            {
+                e.Handled = true;
+
+            }
+        }
+
+        private void Only_Text(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsLetter(e.KeyChar) || (e.KeyChar == (char)Keys.Back) || (e.KeyChar == (char)Keys.Space)))
+            {
+                e.Handled = true;
+
+            }
+
+        }
+
+    }
+}
